86895736c87374fd90ac987bb1e7e347453e18bc
bratelefant <59534030+bratelefant@users.noreply.github.com>
2023-05-06 09:33:22 +0200

    SQL Verbindung

--- README.md
+++ README.md
@@ -13,10 +13,21 @@ Dann brauchen wir Meteor:
 
 # Installation
 
-Um dieses Repo zu installieren muss es zunächst geklont / kopiert werden. Im Ordner wird dann per
+Um dieses Repo zu installieren muss es zunächst geklont / kopiert werden.
+
+Damit die App sich mit dem SQL Server verbinden kann, müssen wir wissen, unter welche Adresse
+der SQL Server erreichbar ist. Außerdem benötigen wir den Benutzernamen und das Passwort. Dann ist es
+noch nötig, den Namen der Datenbank anzugeben, die wir für die App benutzen wollen.
+
+Da es sich z.B. beim Passwort um geschützte Daten handelt, die nicht in diesem öffentlichen Repo landen
+sollten, machen wird das so:
+
+Erstelle eine Kopie von `settings.json.example` unter dem Namen `settings.json`. Diese Datei wird nicht
+auf github geteilt. Hier können dann die SQL-Zugangsdaten eingetragen werden.
+
+Im Ordner kann nun per
 
     meteor npm install
-    meteor run
+    meteor run --settings settings.json
 
-die App gestartet und ist dann unter `http://localhost:3000` erreichbar.
-
+die App gestartet werden und ist dann unter `http://localhost:3000` erreichbar.
--- server/main.js
+++ server/main.js
@@ -1,31 +1,47 @@
-import { Meteor } from 'meteor/meteor';
-import { LinksCollection } from '/imports/api/links';
+import { Meteor } from "meteor/meteor";
+import { LinksCollection } from "/imports/api/links";
+import mysql from "mysql2";
 
 async function insertLink({ title, url }) {
   await LinksCollection.insertAsync({ title, url, createdAt: new Date() });
 }
 
+const initMySql = () => {
+  var con = mysql.createConnection({
+    host: Meteor.settings.mysql.host,
+    user: Meteor.settings.mysql.username,
+    password: Meteor.settings.mysql.password,
+  });
+
+  con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+};
+
 Meteor.startup(async () => {
+  initMySql();
+
   // If the Links collection is empty, add some data.
-  if (await LinksCollection.find().countAsync() === 0) {
+  if ((await LinksCollection.find().countAsync()) === 0) {
     await insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://www.meteor.com/tutorials/react/creating-an-app',
+      title: "Do the Tutorial",
+      url: "https://www.meteor.com/tutorials/react/creating-an-app",
     });
 
     await insertLink({
-      title: 'Follow the Guide',
-      url: 'https://guide.meteor.com',
+      title: "Follow the Guide",
+      url: "https://guide.meteor.com",
     });
 
     await insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com',
+      title: "Read the Docs",
+      url: "https://docs.meteor.com",
     });
 
     await insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com',
+      title: "Discussions",
+      url: "https://forums.meteor.com",
     });
   }
 
