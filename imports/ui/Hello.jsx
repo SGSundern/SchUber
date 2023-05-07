9f69f4b0a9395f680647560ee160dce626c4bbff
bratelefant <59534030+bratelefant@users.noreply.github.com>
2023-05-07 09:22:23 +0200

    Material UI als Frontend und ein Beispiel Request

--- imports/ui/Hello.jsx
+++ imports/ui/Hello.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 export const Hello = () => {
   const [counter, setCounter] = useState(0);
@@ -8,11 +11,17 @@ export const Hello = () => {
   };
 
   return (
-    <div>
-      <button onClick={increment}>Cookie Clicker</button>
-      <p>
-        Hier kansnt du testen, wie React funktioniert: {counter} mal geklickt.
-      </p>
-    </div>
+    <Grid container>
+      <Grid item xs={12} sm={6}>
+        <Button onClick={increment} variant="contained">
+          Cookie Clicker
+        </Button>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Typography>
+          Hier kansnt du testen, wie React funktioniert: {counter} mal geklickt.
+        </Typography>
+      </Grid>
+    </Grid>
   );
 };
--- imports/ui/Links.jsx
+++ imports/ui/Links.jsx
@@ -1,6 +1,4 @@
-import React from "react";
-import { useFind, useSubscribe } from "meteor/react-meteor-data";
-import { LinksCollection } from "../api/links";
+import React, { useEffect } from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import List from "@mui/material/List";
@@ -9,6 +7,9 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import Box from "@mui/material/Box";
+import CircularProgress from "@mui/material/CircularProgress";
+import { useAPI } from "../hooks/useAPI";
 
 /**
  * Diese Komponente zeigt, wie der Data Tracker Datensätze aus
@@ -17,22 +18,29 @@ import OpenInNewIcon from "@mui/icons-material/OpenInNew";
  * Hat nichts mit SQL zu tun.
  * @returns Komponente
  */
-export const Info = () => {
-  const isLoading = useSubscribe("links");
-  const links = useFind(() => LinksCollection.find());
+export const Links = () => {
+  const { loading, data } = useAPI("http://localhost:3000/api/v1/links", "GET");
 
-  if (isLoading()) {
-    return <div>Loading...</div>;
+  if (loading) {
+    return (
+      <Box>
+        <CircularProgress />
+      </Box>
+    );
   }
-
   return (
     <Grid container>
       <Grid item xs={12}>
         <Typography variant="h2">Learn Meteor!</Typography>
       </Grid>
       <Grid item xs={12}>
+        <Typography variant="h5">
+          Diese Daten kommen aus der SQL Datenbank:
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
         <List>
-          {links.map((link) => (
+          {data.map((link) => (
             <ListItem key={link._id}>
               <ListItemIcon>
                 <OpenInNewIcon />
